6635e6fc6288d21c2379f13afc547002da10147f
sjdlabgroup <47805403+sjdlabgroup@users.noreply.github.com>
2026-03-07 20:34:32 -0500

    Update cplx_events.r

--- R/cplx_events.r
+++ R/cplx_events.r
@@ -1,4 +1,4 @@
-#' Extract intricate blocks corresponding to each complex event
+#' Extract intricate blocks corresponding to each complex event.
 #' This function takes a dataframe of genomic blocks and extracts intricate blocks corresponding to each complex event. It identifies complex events by iteratively collecting interacting blocks and contigs. Both redundant and non-redundant complex structural variations (SVs) are identified and returned.
 #' @param df A dataframe containing genomic blocks and their interactions.
 #' @param threshold The minimum threshold for the number of interactions required to initiate a complex event.
